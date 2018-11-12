0a200be07c0d2b68519a01a1efd5d328d2e30203
Alina Zaitseva <zaitseva.alina@steelkiwi.com>
2018-11-12 12:07:17 +0200

    added project

---
+++ AnimationSample/AnimationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationSample
+//
+//  Created by Alina Zaitseva on 11/12/18.
+//  Copyright © 2018 Alina Zaitseva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
